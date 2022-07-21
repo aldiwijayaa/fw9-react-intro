b8152c9338b41461b24bb8ed011e666e45a7e027
aldiwijayaa <m.aldiwijaya97@gmail.com>
2022-07-21 18:15:50 +0700

    add image and name from axios

--- src/App.js
+++ src/App.js
@@ -1,27 +1,70 @@
-import React from "react"
-import {Button} from "react-bootstrap"
+// import React from "react";
+// import axios from "axios";
+// import { Col, Container, Row } from "react-bootstrap";
 
-const Count = ({number}) =>{
-  React.useEffect(()=>{
-    console.log(number);
-  },[number])
-    return(
-    <>
-    <h1 className="my-auto">{number}</h1>
-    </>)
-}
+// const api = axios.create({
+//   baseURL:'https://rickandmortyapi.com/api/character'
+// })
+
+// class App extends React.Component{
+//   state={
+//     data:[]
+//   }
+
+//   constructor() {
+//     super();
+//     api.get('/').then(res => {
+//       console.log(res.data.results);
+//       this.setState({data: res.data.results})
+//     })
+//   }
+
+//   render(){
+//     return(
+//       <Container className="w-100">
+//         <Row className="d-flex justify-content-between">
+//           {this.state.data.map(result=>
+//           <Col s={12} md={3} key={result.id}>
+//             <img className="w-100" src={result.image} alt={result.name}></img>
+//             <h2 className="text-center">{result.name}</h2>
+//             </Col>)}
+//         </Row>
+//       </Container>
+//     )
+//   }
+// }
+
+// export default App
+
+import React from 'react'
+import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
-  const [value, increment] =React.useState(0)
+  let [data, setData] = React.useState(null)
+
+  React.useEffect(() => {
+    const Auth = async () => {
+      try {
+        const res = await axios.get('https://rickandmortyapi.com/api/character');
+        setData(res.data.results)
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    Auth()
+  }, [])
+
   return(
-    <div className="parent d-flex justify-content-center alight-items-center">
-      <div className="d-flex my-auto">
-        <Button className="mx-5 size-btn" onClick={()=>value===0?increment(value+0):increment(value-1)}>Decrement</Button>
-        {<Count number={value}></Count>}
-        <Button className="mx-5 size-btn" onClick={()=>increment(value+1)}>increment</Button>
-      </div>
-    </div>
+    <main className='vh-100 d-flex flex-row gap-3 justify-content-center align-items-center flex-wrap'>
+      {!data? null:data.map((result) => (
+        <section className='d-flex flex-column justify-content-center align-items-center col-2 p-3'>
+          <img className='col-12' alt='' src={result.image}></img>
+          <span className=''>{result.name}</span>
+        </section>
+      ))}
+    </main>
   )
-}
+} 
 
 export default App
